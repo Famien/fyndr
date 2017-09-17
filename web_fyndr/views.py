e05b24bd4fadb299fd1762ca494bfb00f1b06d2f
weilian@mit.edu <weilianchu@Weilians-MacBook-Pro.local>
2017-09-17 01:01:40 -0400

    testing db connection

--- web_fyndr/views.py
+++ web_fyndr/views.py
@@ -4,7 +4,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 from django.core.files.storage import FileSystemStorage
+import psycopg2
 
+try:
+	conn = psycopg2.connect("dbname='d9eu2mf4n8hcq4' user='oevfewnurxkiyq' host='ec2-23-21-186-138.compute-1.amazonaws.com' password='59e1ccde40a94e32af13a27b3866969c92b83a41474c4b3cde782348cf8985ca'")
+except:
+	print "can't connect to database"
 
 def index(request):
 	print("index")
@@ -33,3 +38,11 @@ def map(request):
 
 def simple_upload(request):
 	print("simple ")
+
+def find_rooms(request):
+	cur = conn.cursor()
+	cur.execute("""SELECT * FROM fyndr_rooms""")
+	rows = cur.fetchall()
+	return rows
+
+
